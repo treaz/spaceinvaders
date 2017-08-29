209836de7eb0fad995b8771e51a9f6c114e6f97c
Horia Constantin <h.constantin@gmail.com>
2017-08-29 15:24:46 +0200

    added tests for MatrixUtils.stringMatrixToRealMatrix and refactored a bit

--- src/main/java/com/horiaconstantin/invaders/MatrixUtils.java
+++ src/main/java/com/horiaconstantin/invaders/MatrixUtils.java
@@ -1,9 +1,13 @@
 package com.horiaconstantin.invaders;
 
+import lombok.NonNull;
 import org.apache.commons.math3.exception.NotStrictlyPositiveException;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 
+/**
+ * Class that contains all kind of utilities for matrix manipulation specific to this app
+ */
 public class MatrixUtils {
 
     /**
@@ -20,21 +24,44 @@ public class MatrixUtils {
         return realMatrixToStringMatrix(emptyMatrix);
     }
 
-    //TODO javadoc this
-    public static RealMatrix stringMatrixToRealMatrix(String[][] radarImage){
-        double[][] radarImageAsNumbers = new double[radarImage.length][radarImage[0].length];
-        for (int i = 0; i<radarImage.length; i++){
-            for (int j = 0; j<radarImage[0].length; j++){
-                radarImageAsNumbers[i][j] = ("-".equals(radarImage[i][j]) ? 0 : 1);
+    /**
+     * Transforms a String[][] into a RealMatrix
+     * @param stringMatrix a non-null matrix that has only "o" or "-" as values
+     * @return a RealMatrix that has 0 in the positions in which stringMatrix has "-" and "1" in the positions in which it has "o"
+     * @throws IllegalArgumentException if argument contains other values than "o" or "-"
+     * @throws NullPointerException if argument is null
+     */
+    public static RealMatrix stringMatrixToRealMatrix(String[][] stringMatrix){
+        validateValues(stringMatrix);
+
+        double[][] radarImageAsNumbers = new double[stringMatrix.length][stringMatrix[0].length];
+        for (int i = 0; i<stringMatrix.length; i++){
+            for (int j = 0; j<stringMatrix[0].length; j++){
+                radarImageAsNumbers[i][j] = ("-".equals(stringMatrix[i][j]) ? 0 : 1);
             }
         }
 
         return new Array2DRowRealMatrix(radarImageAsNumbers);
     }
 
+    /**
+     * Validates is the argument contains valid values: "o" or "-"
+     * @param stringMatrix
+     */
+    public static void validateValues(@NonNull String[][] stringMatrix) {
+        for (String[] row : stringMatrix) {
+            for (String value : row) {
+                if (!"-".equals(value) && !"o".equals(value)){
+                    throw new IllegalArgumentException("Radar image contains invalid characters");
+                }
+            }
+        }
+    }
+
     //TODO javadoc this
-    public static String[][] realMatrixToStringMatrix(RealMatrix radarImageMartix){
-        double[][] radarImageAsNumbers = radarImageMartix.getData();
+    public static String[][] realMatrixToStringMatrix(@NonNull RealMatrix doubleMatrix){
+        //TODO validate values
+        double[][] radarImageAsNumbers = doubleMatrix.getData();
 
         String[][] radarImage = new String[radarImageAsNumbers.length][radarImageAsNumbers[0].length];
         for (int i = 0; i<radarImageAsNumbers.length; i++){
--- src/test/java/com/horiaconstantin/invaders/MatrixUtilsTest.java
+++ src/test/java/com/horiaconstantin/invaders/MatrixUtilsTest.java
@@ -1,6 +1,8 @@
 package com.horiaconstantin.invaders;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.math3.exception.NotStrictlyPositiveException;
+import org.apache.commons.math3.linear.RealMatrix;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -9,17 +11,17 @@ import static org.junit.Assert.*;
 public class MatrixUtilsTest {
 
     @Test(expected = NotStrictlyPositiveException.class)
-    public void getEmptyMatrixWithNegativeArguments() throws Exception {
+    public void getEmptyMatrixWithNegativeArguments() {
         MatrixUtils.getEmptyMatrix(-1, -1);
     }
 
     @Test(expected = NotStrictlyPositiveException.class)
-    public void getEmptyMatrixWithZeroColumns() throws Exception {
+    public void getEmptyMatrixWithZeroColumns() {
         MatrixUtils.getEmptyMatrix(10, 0);
     }
 
     @Test
-    public void getEmptyMatrix() throws Exception {
+    public void getEmptyMatrix() {
         String[][] emptyMatrix = MatrixUtils.getEmptyMatrix(30, 25);
 
         assertThat(emptyMatrix[0][0], is("-"));
@@ -27,10 +29,42 @@ public class MatrixUtilsTest {
     }
 
 
-//    @Test
-//    public void stringMatrixToRealMatrix() throws Exception {
-//        MatrixUtils.stringMatrixToRealMatrix()
-//    }
+    @Test(expected = NullPointerException.class)
+    public void stringMatrixToRealMatrix() {
+        MatrixUtils.stringMatrixToRealMatrix(null);
+        fail();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void stringMatrixToRealMatrix_nullValues() {
+        String[][] matrix = new String[1][1];
+
+        MatrixUtils.stringMatrixToRealMatrix(matrix);
+        fail();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void stringMatrixToRealMatrix_incorrectValueThrowsException() {
+        String[][] matrix = new String[1][1];
+        matrix[0][0] = "x";
+
+        MatrixUtils.stringMatrixToRealMatrix(matrix);
+
+        fail();
+    }
+
+    @Test
+    public void stringMatrixToRealMatrix_correctConversion() {
+        String[][] matrix = new String[1][2];
+        matrix[0][0] = "o";
+        matrix[0][1] = "-";
+
+        RealMatrix realMatrix = MatrixUtils.stringMatrixToRealMatrix(matrix);
+
+        assertThat(realMatrix.getEntry(0,0), is(1.0));
+        assertThat(realMatrix.getEntry(0,1), is(0.0));
+    }
+
 //
 //    @Test
 //    public void realMatrixToStringMatrix() throws Exception {
